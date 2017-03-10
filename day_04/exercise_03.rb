e8caf35579a87e1223abad87c3fb92c221ca38f7
lvtho <lvtho129@gmail.com>
2017-03-10 20:05:13 +0700

    fix-day04

--- day_04/exercise_03.rb
+++ day_04/exercise_03.rb
@@ -4,21 +4,23 @@ class Ex3
   def input_attribute
     puts "Enter param 1: "
     param1 = gets.chomp.to_i
-    check_integer param1
-    puts "Enter param 2: "
-    param2 = gets.chomp.to_i
-    check_integer param2
-    puts "add(param1, param2) = #{param1+param2}"
-  end
-
-  def check_integer value
-    while value == 0
+    while param1 == 0
       puts "Please enter an integer number:"
-      value = gets.chomp.to_i
-      if value != 0
+      param1 = gets.chomp.to_i
+      if param1 != 0
         break
       end
     end
+    puts "Enter param 2: "
+    param2 = gets.chomp.to_i
+    while param2 == 0
+      puts "Please enter an integer number:"
+      param2 = gets.chomp.to_i
+      if param2 != 0
+        break
+      end
+    end
+    puts "add(param1, param2) = #{param1+param2}"
   end
 
   ex3 = Ex3.new.input_attribute
--- day_04/exercise_04.rb
+++ day_04/exercise_04.rb
@@ -5,23 +5,12 @@ class Ex4
     while true do
       puts "Enter a year : "
       year = gets.chomp.to_i
-      du = year%100
-      if du > 0
+      if year%100 > 0
         centuryFromYear = year/100 +1
       else
         centuryFromYear = year/100
       end
       puts "centuryFromYear(#{year}): #{centuryFromYear}"
-    end
-  end
-
-  def check_integer value
-    while value == 0
-      puts "Please enter an integer number:"
-      value = gets.chomp.to_i
-      if value != 0
-        break
-      end
     end
   end
 
